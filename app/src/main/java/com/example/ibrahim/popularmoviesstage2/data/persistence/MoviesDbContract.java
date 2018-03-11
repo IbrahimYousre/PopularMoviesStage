974cd496c3f7f584730d216527cf2fb0ed8ef407
Ibrahim Yousre <ib.yousre@gmail.com>
2018-03-11 23:26:00 +0400

    Update DB

--- app/src/main/java/com/example/ibrahim/popularmoviesstage2/data/persistence/MoviesDbContract.java
+++ app/src/main/java/com/example/ibrahim/popularmoviesstage2/data/persistence/MoviesDbContract.java
@@ -1,5 +1,8 @@
 package com.example.ibrahim.popularmoviesstage2.data.persistence;
 
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -8,12 +11,19 @@ import android.provider.BaseColumns;
 
 public final class MoviesDbContract {
 
+    public static final String AUTHORITY = "com.example.ibrahim.popularmoviesstage2";
+
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + AUTHORITY);
+
     private MoviesDbContract() {
     }
 
     public static class FavoriteMoviesTable implements BaseColumns {
 
         public static final String TABLE_NAME = "favorite_movies";
+
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon()
+                .appendPath(TABLE_NAME).build();
 
         public static final String COLUMN_API_ID = "api_id";
         public static final String COLUMN_TITLE = "title";
@@ -23,5 +33,15 @@ public final class MoviesDbContract {
         public static final String COLUMN_DURATION = "duration";
         public static final String COLUMN_RATING = "rating";
         public static final String COLUMN_STORY = "story";
+
+        public static final String CONTENT_DIR_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + AUTHORITY + "/" + TABLE_NAME;
+        // create cursor of base type item for single entry
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + AUTHORITY + "/" + TABLE_NAME;
+
+        public static Uri buildUriWithApiId(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
     }
 }
--- app/src/main/java/com/example/ibrahim/popularmoviesstage2/data/persistence/MoviesDbHelper.java
+++ app/src/main/java/com/example/ibrahim/popularmoviesstage2/data/persistence/MoviesDbHelper.java
@@ -10,7 +10,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class MoviesDbHelper extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "movies_db";
-    public static final int DATABASE_VERSION = 1;
+    public static final int DATABASE_VERSION = 2;
 
     public MoviesDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -20,14 +20,15 @@ public class MoviesDbHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("CREATE TABLE " + MoviesDbContract.FavoriteMoviesTable.TABLE_NAME + "(" +
                 MoviesDbContract.FavoriteMoviesTable._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                MoviesDbContract.FavoriteMoviesTable.COLUMN_API_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                MoviesDbContract.FavoriteMoviesTable.COLUMN_API_ID + " INTEGER NOT NULL, " +
                 MoviesDbContract.FavoriteMoviesTable.COLUMN_TITLE + " TEXT NOT NULL, " +
                 MoviesDbContract.FavoriteMoviesTable.COLUMN_POSTER_PATH + " TEXT NOT NULL, " +
                 MoviesDbContract.FavoriteMoviesTable.COLUMN_BACKDROP_PATH + " TEXT NOT NULL, " +
                 MoviesDbContract.FavoriteMoviesTable.COLUMN_RELEASE_YEAR + " TEXT NOT NULL, " +
                 MoviesDbContract.FavoriteMoviesTable.COLUMN_DURATION + " TEXT DEFAULT 'Unkown', " +
                 MoviesDbContract.FavoriteMoviesTable.COLUMN_RATING + " TEXT NOT NULL, " +
-                MoviesDbContract.FavoriteMoviesTable.COLUMN_STORY + " TEXT NOT NULL" +
+                MoviesDbContract.FavoriteMoviesTable.COLUMN_STORY + " TEXT NOT NULL, " +
+                "CONSTRAINT api_id_unique UNIQUE (" + MoviesDbContract.FavoriteMoviesTable.COLUMN_API_ID + ")" +
                 ");");
     }
 
