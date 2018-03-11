41eb451e41e071fb867a73e52a1ad0cc07b2c2bc
Ibrahim Yousre <ib.yousre@gmail.com>
2018-03-11 19:55:46 +0400

    Initial Commit

---
+++ app/src/main/java/com/example/ibrahim/popularmoviesstage2/DetailActivity.java
@@ -0,0 +1,24 @@
+package com.example.ibrahim.popularmoviesstage2;
+
+import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        CollapsingToolbarLayout toolbarLayout = findViewById(R.id.collapsingToolbarLayout);
+        toolbarLayout.setTitle("Zootopia");
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) actionBar.setDisplayHomeAsUpEnabled(true);
+
+    }
+
+}
