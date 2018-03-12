f9f880028cebd6ae1310938d7a23ac6cf4b0670d
Ibrahim Yousre <ib.yousre@gmail.com>
2018-03-12 23:41:29 +0400

    Setup Stetho to work with picasso

--- app/src/main/java/com/example/ibrahim/popularmoviesstage2/MyApplication.java
+++ app/src/main/java/com/example/ibrahim/popularmoviesstage2/MyApplication.java
@@ -3,6 +3,11 @@ package com.example.ibrahim.popularmoviesstage2;
 import android.app.Application;
 
 import com.facebook.stetho.Stetho;
+import com.facebook.stetho.okhttp3.StethoInterceptor;
+import com.jakewharton.picasso.OkHttp3Downloader;
+import com.squareup.picasso.Picasso;
+
+import okhttp3.OkHttpClient;
 
 /**
  * Created by ibrahim on 3/12/18.
@@ -13,5 +18,16 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
         Stetho.initializeWithDefaults(this);
+
+        OkHttpClient okClient = new OkHttpClient
+                .Builder()
+                .addNetworkInterceptor(new StethoInterceptor())
+                .build();
+
+        Picasso picasso = new Picasso.Builder(this)
+                .downloader(new OkHttp3Downloader(okClient))
+                .build();
+
+        Picasso.setSingletonInstance(picasso);
     }
 }
