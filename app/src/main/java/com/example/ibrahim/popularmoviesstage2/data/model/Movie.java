34b0f43410a87a1f20a42714d20f1266b79e7044
Ibrahim Yousre <ib.yousre@gmail.com>
2018-03-13 00:48:01 +0400

    Update the model to match the database

--- app/src/main/java/com/example/ibrahim/popularmoviesstage2/data/model/Movie.java
+++ app/src/main/java/com/example/ibrahim/popularmoviesstage2/data/model/Movie.java
@@ -7,46 +7,24 @@ import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 import java.io.Serializable;
-import java.util.List;
 
 public class Movie implements Serializable, Parcelable {
 
-    @SerializedName("vote_count")
-    @Expose
-    public long voteCount;
     @SerializedName("id")
     @Expose
     public long id;
-    @SerializedName("video")
-    @Expose
-    public boolean video;
     @SerializedName("vote_average")
     @Expose
     public float voteAverage;
     @SerializedName("title")
     @Expose
     public String title;
-    @SerializedName("popularity")
-    @Expose
-    public float popularity;
     @SerializedName("poster_path")
     @Expose
     public String posterPath;
-    @SerializedName("original_language")
-    @Expose
-    public String originalLanguage;
-    @SerializedName("original_title")
-    @Expose
-    public String originalTitle;
-    @SerializedName("genre_ids")
-    @Expose
-    public List<Long> genreIds = null;
     @SerializedName("backdrop_path")
     @Expose
     public String backdropPath;
-    @SerializedName("adult")
-    @Expose
-    public boolean adult;
     @SerializedName("overview")
     @Expose
     public String overview;
@@ -75,41 +53,29 @@ public class Movie implements Serializable, Parcelable {
 
     private final static long serialVersionUID = -2692851914400425098L;
 
-    protected Movie(Parcel in) {
-        this.voteCount = ((long) in.readValue((long.class.getClassLoader())));
+    private Movie(Parcel in) {
         this.id = ((long) in.readValue((long.class.getClassLoader())));
-        this.video = ((boolean) in.readValue((boolean.class.getClassLoader())));
         this.voteAverage = ((float) in.readValue((float.class.getClassLoader())));
         this.title = ((String) in.readValue((String.class.getClassLoader())));
-        this.popularity = ((float) in.readValue((float.class.getClassLoader())));
         this.posterPath = ((String) in.readValue((String.class.getClassLoader())));
-        this.originalLanguage = ((String) in.readValue((String.class.getClassLoader())));
-        this.originalTitle = ((String) in.readValue((String.class.getClassLoader())));
-        in.readList(this.genreIds, (java.lang.Long.class.getClassLoader()));
         this.backdropPath = ((String) in.readValue((String.class.getClassLoader())));
-        this.adult = ((boolean) in.readValue((boolean.class.getClassLoader())));
         this.overview = ((String) in.readValue((String.class.getClassLoader())));
         this.releaseDate = ((String) in.readValue((String.class.getClassLoader())));
+        this.duration = ((String) in.readValue((String.class.getClassLoader())));
     }
 
     public Movie() {
     }
 
     public void writeToParcel(Parcel dest, int flags) {
-        dest.writeValue(voteCount);
         dest.writeValue(id);
-        dest.writeValue(video);
         dest.writeValue(voteAverage);
         dest.writeValue(title);
-        dest.writeValue(popularity);
         dest.writeValue(posterPath);
-        dest.writeValue(originalLanguage);
-        dest.writeValue(originalTitle);
-        dest.writeList(genreIds);
         dest.writeValue(backdropPath);
-        dest.writeValue(adult);
         dest.writeValue(overview);
         dest.writeValue(releaseDate);
+        dest.writeValue(duration);
     }
 
     public int describeContents() {
